be7bf80bae1dc0ac3b80ada44834a57eef11e1e7
jumarin22 <jumarin22@users.noreply.github.com>
2026-09-16 00:13:45 +0000

    deploy: 781c45b1c017f92a0d29d170368a748493570518

---
+++ assets/js/814f3328.0ee28e56.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[7472],{5513(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/justinus/journal/welcome","unlisted":false,"date":"2026-09-16T00:12:41.000Z"}]}')}}]);
