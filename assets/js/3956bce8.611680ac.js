9c8f1194d58bc6e4170bd8d832c49e9c02aceb38
jumarin22 <jumarin22@users.noreply.github.com>
2026-09-21 22:02:42 +0000

    deploy: c8caad4420924fff802c87e9fc2609b6f72cbe80

--- assets/js/3956bce8.611680ac.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[3416],{2725(e){e.exports=JSON.parse('{"authors":[{"name":"Justinus","url":"https://jumarin22.github.io/my-website","key":"me","page":null,"count":1}]}')}}]);
---
+++ assets/js/acecf23e.30116836.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/justinus/journal","blogTitle":"Blog","authorsListPath":"/justinus/journal/authors"}')}}]);
